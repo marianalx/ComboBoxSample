80a77ab1225ff1ba5d7bc52cebc729357a3a71f9
MW <marian.witkowski@gmail.com>
2020-04-22 17:56:09 +0200

    initial commit

---
+++ ComboBoxSample/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ComboBoxSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Czarny",
+            "Brązowy",
+            "Czerwony",
+            "Pomarańczowy",
+            "Żółty",
+            "Zielony",
+            "Niebieski",
+            "Fiotelowy",
+            "Szary",
+            "Biały",
+            "Złoty",
+            "Srebrny"});
+            this.comboBox1.Location = new System.Drawing.Point(214, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(627, 63);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(805, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label z danymi odczytanymi z combo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(124, 292);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(795, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1032, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ComboBoxSample/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex>-1)
+            {
+                label1.Text = String.Format("Pozycja {0} - kolor {1}",
+                    comboBox1.SelectedIndex, comboBox1.SelectedItem);
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        panel1.BackColor = Color.Black;
+                        break;
+                    case 1:
+                        panel1.BackColor = Color.Brown;
+                        break;
+                    case 2:
+                        panel1.BackColor = Color.Red;
+                        break;
+                    case 3:
+                        panel1.BackColor = Color.Orange;
+                        break;
+                    case 4:
+                        panel1.BackColor = Color.Yellow;
+                        break;
+                    case 5:
+                        panel1.BackColor = Color.Green;
+                        break;
+                    case 6:
+                        panel1.BackColor = Color.Blue;
+                        break;
+                    case 7:
+                        panel1.BackColor = Color.Violet;
+                        break;
+                    case 8:
+                        panel1.BackColor = Color.Gray;
+                        break;
+                    case 9:
+                        panel1.BackColor = Color.AntiqueWhite;
+                        break;
+                    case 10:
+                        panel1.BackColor = Color.Gold;
+                        break;
+                    case 11:
+                        panel1.BackColor = Color.Silver;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
